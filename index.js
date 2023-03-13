cc8968bd7aa633b479f936fece341246c7d7b6ba
Cristian Basualdo <crisbasualdo86@gmail.com>
2023-03-12 23:41:10 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// Directorio Público
+app.use(express.static('public'));
+
+// Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+
+// escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
